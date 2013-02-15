d8b5d593b73a250d328dbe9dc730c107464ed8ef
Bashir Alfitori <bashir.alfitori@gmail.com>
2013-02-15 14:48:29 +0100

    Make a basic User Model (including secure passwords)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def home
+
+  end
+  
+
+  def help
+
+  end
+  
+
+  def about
+
+  end
+  
+
+  def contact
+  	
+  end
+
 end
